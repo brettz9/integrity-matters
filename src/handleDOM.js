1a25d48458e68bf4c806e86c6f148bfc9019d599
Brett Zamir <brettz9@yahoo.com>
2020-07-29 12:22:10 +0800

    - Testing: Add comment to ignore presumably unreachable code

--- src/handleDOM.js
+++ src/handleDOM.js
@@ -16,6 +16,12 @@ function handleDOM (domString) {
   // eslint-disable-next-line promise/avoid-new -- Has no Promise API
   return new Promise((resolve, reject) => {
     const handler = new DomHandler(function (error, dom) {
+      // Per https://github.com/fb55/htmlparser2/search?q=onerror&unscoped_q=onerror ,
+      //   including the one more relevant source: https://github.com/fb55/htmlparser2/blob/0189e56a876e17c60be5ff7433bcfea3756f9ab9/src/Tokenizer.ts
+      //  ...it seems `onerror` should never occur based on our usage (i.e.,
+      //  we are not calling `end` or `write` after `end` and states
+      //  will presumably not be unknown)
+      // istanbul ignore if
       if (error) {
         reject(error);
         return;
