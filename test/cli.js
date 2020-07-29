f5da9f31a2919d1fb288cc8af30fb9a356b97f29
Brett Zamir <brettz9@yahoo.com>
2020-07-29 17:38:29 +0800

    - Testing: JSON and `fallback`

--- test/cli.js
+++ test/cli.js
@@ -74,6 +74,9 @@ const badIntegrityGoodVersionResult = getFixturePath(
 const fallbackResult = getFixturePath(
   'fallback-result.html'
 );
+const fallbackResultJSON = getFixturePath(
+  'fallback-result.json'
+);
 const addCrossoriginPath = getFixturePath(
   'result-addCrossorigin.html'
 );
@@ -573,53 +576,66 @@ describe('Binary', function () {
       }
     );
 
-    it(
-      'should work with `fallback` and `globalCheck`',
-      async function () {
-        const {stdout, stderr} = await execFile(
-          binFile,
-          [
-            '--ignoreURLFetches',
-            '--fallback',
-            '--globalCheck',
-            'leaflet=script=window.Leaflet',
-            '--globalCheck',
-            'leaflet=link=window.SomeLeafletCSSCheck',
-            '--file',
-            'test/fixtures/fallback.html',
-            '--outputPath', outputPath
-          ],
-          {
-            timeout: 15000
-          }
-        );
+    [
+      true,
+      false
+    ].forEach((json) => {
+      it(
+        'should work with `fallback` and `globalCheck`' +
+          (json ? ' (JSON)' : ''),
+        async function () {
+          const {stdout, stderr} = await execFile(
+            binFile,
+            [
+              '--ignoreURLFetches',
+              '--fallback',
+              '--globalCheck',
+              'leaflet=script=window.Leaflet',
+              '--globalCheck',
+              'leaflet=link=window.SomeLeafletCSSCheck',
+              '--file',
+              json
+                ? 'test/fixtures/fallback.json'
+                : 'test/fixtures/fallback.html',
+              '--outputPath', outputPath
+            ],
+            {
+              timeout: 15000
+            }
+          );
 
-        // console.log('stdout', stdout);
-        // console.log('stderr', stderr);
+          // console.log('stdout', stdout);
+          // console.log('stderr', stderr);
 
-        expect(stdout).to.contain(
-          `INFO: Finished writing to ${outputPath}\n`
-        );
+          expect(stdout).to.contain(
+            `INFO: Finished writing to ${outputPath}\n`
+          );
 
-        expect(stderr).to.match(new RegExp(
-          escStringRegex(
-            `WARNING: The URL's version (1.4.0) is less than the ` +
-              `devDependency "leaflet"'s current '\`package.json\` range, ` +
-              `"${devDependencies.leaflet}". Checking \`node_modules\` for a ` +
-              `valid installed version to update the URL...\n` +
-            `WARNING: The lock file version ${lockDeps.leaflet.version} is ` +
-              `greater for package "leaflet" than the URL version 1.4.0. ` +
-              `Checking \`node_modules\` for a valid installed version to ` +
-              `update the URL...\n`
-          ),
-          'u'
-        ));
+          expect(stderr).to.match(new RegExp(
+            escStringRegex(
+              `WARNING: The URL's version (1.4.0) is less than the ` +
+                `devDependency "leaflet"'s current '\`package.json\` range, ` +
+                `"${devDependencies.leaflet}". Checking \`node_modules\` ` +
+                `for a valid installed version to update the URL...\n` +
+              `WARNING: The lock file version ${lockDeps.leaflet.version} ` +
+                `is greater for package "leaflet" than the URL version ` +
+                `1.4.0. Checking \`node_modules\` for a valid installed ` +
+                `version to update the URL...\n`
+            ),
+            'u'
+          ));
 
-        const contents = await readFile(outputPath, 'utf8');
-        const expected = await readFile(fallbackResult, 'utf8');
-        expect(contents).to.equal(expected);
-      }
-    );
+          const contents = await readFile(outputPath, 'utf8');
+          const expected = await readFile(
+            json
+              ? fallbackResultJSON
+              : fallbackResult,
+            'utf8'
+          );
+          expect(contents).to.equal(expected);
+        }
+      );
+    });
 
     it(
       'should work with `forceIntegrityChecks`',
